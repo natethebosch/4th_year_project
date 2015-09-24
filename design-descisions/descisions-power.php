e4b67c0b56f0e61920e0f8611b5531473633cd4d
Nathan Bosscher <nate.bosscher@gmail.com>
2015-09-24 19:14:19 -0400

    Add Power and Setup descisions

---
+++ design-descisions/descisions-power.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @Author: Nate Bosscher
+ * @Date:   2015-09-24 19:02:47
+ * @Last Modified by:   Nate Bosscher
+ * @Last Modified time: 2015-09-24 19:05:34
+ */
+
+$power = new Descision("Power");
+$power
+	->addOption(function(){
+		$opt = new Option("Battery");
+		$opt
+			->addReliesOn("Weather")
+			->addCon("Poor performance in cold weather")
+			->addPro("No wired power required");
+			->addPro("Easy setup");
+
+		return $opt;
+	})
+	->addOption(function(){
+		$opt = new Option("120VAC");
+		$opt
+			->addReliesOn("Power Access")
+			->addPro("Reliable");
+
+		return $opt;
+	});
