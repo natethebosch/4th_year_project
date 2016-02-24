26d5cd998f8613d2d202048187cee8be093d8070
Nathan Bosscher <nate.bosscher@gmail.com>
2016-02-24 15:24:49 -0500

    ImageProcessor skeleton and sample code Add SensorDataPoint

--- udoo/Model/ImageProcessor.h
+++ udoo/Model/ImageProcessor.h
@@ -9,9 +9,43 @@
 #define _IMAGEPROCESSOR_H
 
 #include "Task.h"
+#include "SensorDataPoint.h";
 
 
 class ImageProcessor: public Task {
+    BlockingQueueReceiver<SensorDataPoint> input;
+    
+public:
+    ImageProcessor(BlockingQueueReceiver<SensorDataPoint> _input): Task("ImageProcessor", 20){
+        input = _input;
+    };
+    
+    void run(void* argc){
+        // image processor main logic goes here
+        /*
+         * Input loop could be implemented as follows
+         * 
+         * SensorDataPoint dp;
+         * 
+         * // infinite loop
+         * for(;;){ 
+         *      // fetch from the buffer
+         *
+         *      try{
+         *         dp = input.take();
+         *         process(dp);
+         *      }catch(BlockingQueueStatus s){
+         *          if(s == BLOCKING_QUEUE_TIMEOUT){
+         *              continue;
+         *          }else{
+         *              Debug::output("Irrecoverable error. Exiting...");
+         *              exit(-1);
+         *          }
+         *      }
+         * }
+         * 
+         */
+    }
 };
 
 #endif //_IMAGEPROCESSOR_H
