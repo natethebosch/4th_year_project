076207ab517e83d0bbe87f5fd0cda394e45d8cee
AlphaFlight <king.tut@rogers.com>
2016-02-25 12:26:31 -0500

    removed imageprocessor header

--- udoo/Model/tasks/ImageProcessor.h
+++ udoo/Model/tasks/ImageProcessor.h
@@ -11,7 +11,53 @@
 #include "../sys/Task.h"
 #include "../helpers/BlockingQueue.h"
 #include "../type/SensorDataPoint.h";
-#include "ImageProcessorHeader"
+
+#define HEIGHT 		500
+#define WIDTH 		480
+#define MAJORXS 	24
+#define MAXREADING 	(255*24)
+#define xSpacing	(WIDTH/MAJORXS)
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <dlib\pixel.h>
+#include <dlib\array2d.h>
+#include <dlib\image_io.h>
+#include <dlib\image_transforms.h>
+
+
+
+using namespace std;
+using namespace dlib;
+
+
+class ImageProcessor {
+	private:
+		int currentX;
+		int rowLength;
+		int *lastY;
+		int *currentY;
+		bool first;
+		int currentXCompile;
+		array2d<hsi_pixel> img;
+		float sensorData[HEIGHT][WIDTH];
+		void yCompile();
+		void xCompileTo(int botY);
+	
+		
+		
+	
+	public:
+		ImageProcessor(int i);
+		void addData (float value, int y);
+		void displayData ();
+		array2d<hsi_pixel>& compileImage();
+		array2d<hsi_pixel>& getImage();
+		
+
+};
+
 
 
 class ImageProcessor: public Task {
