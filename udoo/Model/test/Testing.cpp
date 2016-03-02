161990fd293946083937d20c20aab3676f22c632
Nathan Bosscher <nate.bosscher@gmail.com>
2016-03-02 11:55:50 -0500

    Added task coverage to testing

--- udoo/Model/test/Testing.cpp
+++ udoo/Model/test/Testing.cpp
@@ -8,8 +8,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 
 #include "./TBlockingQueue.h"
+#include "./TTask.h"
 
 int main(int argc, char** argv){
     
@@ -18,6 +20,9 @@ int main(int argc, char** argv){
     std::cout << "Starting test sequence\n\n";
     std::cout.flush();
     
+    /**************************
+     * Blocking Queue Test
+     **************************/
     
     TBlockingQueue* tbq = new TBlockingQueue;
     test_number++;
@@ -29,6 +34,21 @@ int main(int argc, char** argv){
     }
     
     delete tbq;
+    
+    /**************************
+     * Task Test
+     **************************/
+    
+    TTask* ttk = new TTask;
+    test_number++;
+    
+    if(!ttk->test()){
+        std::cout << "Test " << test_number << " failed:\n";
+        std::cout << ttk->error << "\n";
+        std::cout << "\n\n";
+    }
+    
+    delete ttk;
     
     std::cout << "Testing Complete!\n";
     
