50799464ba95dd972f5967c9806d024533a85faa
AlphaFlight <king.tut@rogers.com>
2016-03-15 16:00:12 -0400

    arduino code

---
+++ arduino/main/main.ino
@@ -0,0 +1,29 @@
+#define MAXVALUE 
+
+int digitalInputPin= 0;
+int sensorLocations[24]={0};
+double sensorValue=0;
+int movedY = 0;
+int xVal =0;
+int yVal =0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode (digitalInputPin, INPUT);
+  for (int i=0; i<24; i++){
+    pinMode (sensorLocations[i],INPUT);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead (digitalInputPin)==1){
+    for (int i=0; i<24; i++){
+      sensorValue= analogRead(sensorLocations[i]);
+      xVal=i*20;
+      yVal=0;
+
+      
+    }
+  }
+}
